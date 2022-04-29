894b089a94182e65938257e09e5013d8d3849ddf
Thomas John <john.th@northeastern.edu>
2022-04-28 21:46:31 -0400

    person directory for user related queries

--- Buddy/src/main/java/models/Person/Person.java
+++ Buddy/src/main/java/models/Person/Person.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package models;
+package models.Person;
 
 import Business.DeliveryAgency.ClerkDirectory;
 import Business.DeliveryAgency.DriverDirectory;
