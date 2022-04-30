1fe353f56e1a246809bad7ee6a5fa44b82a89033
Thomas John <john.th@northeastern.edu>
2022-04-30 10:47:21 -0400

    Role base files

---
+++ Buddy/src/main/java/models/Role/VolunteerRole.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models.Role;
+
+import javax.swing.JPanel;
+import models.Person.Person;
+
+/**
+ *
+ * @author thomas
+ */
+public class VolunteerRole extends Role{
+    public JPanel createWorkArea(JPanel userProcessContainer, Person account, EcoSystem business) {
+        return new VolunteerAreaJPanel(userProcessContainer, account, business);
+    }
+}
