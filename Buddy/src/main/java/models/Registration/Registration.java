569968b7c8f57f543e0590ea5dd11c0422eab28b
Thomas John <john.th@northeastern.edu>
2022-04-30 14:44:43 -0400

    register data

--- Buddy/src/main/java/models/Registration/Registration.java
+++ Buddy/src/main/java/models/Registration/Registration.java
@@ -9,5 +9,112 @@ package models.Registration;
  * @author thomas
  */
 public class Registration {
+    private String name;
+    private String userName;
+    private String password;
+    private String email;
+    private String carrier;
+    private String phone;
+    private String photo;
+    private String role;
+    private String location;
+    private String status;
     
+    public Registration(String name, String role, String username, String password, String email, String phone, String carrier,  String location, String imagePath, String status){
+        this.name = name;
+        this.role =role;
+        this.userName = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.carrier = carrier;
+        this.location = location;
+        this.photo = imagePath;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }  
+    
+    @Override
+    public String toString(){
+        return name;
+    }
 }
--- Buddy/src/main/java/models/Registration/RegistrationDirectory.java
+++ Buddy/src/main/java/models/Registration/RegistrationDirectory.java
@@ -4,10 +4,45 @@
  */
 package models.Registration;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author thomas
  */
 public class RegistrationDirectory {
+    private ArrayList<Registration> registrationList;
+    private Registration registration;
+    
+    //Getters and Setters
+
+    public ArrayList<Registration> getRegistrationList() {
+        return registrationList;
+    }
+
+    public void setRegistrationList(ArrayList<Registration> registrationList) {
+        this.registrationList = registrationList;
+    }
+    
+
+    public Registration getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(Registration registration) {
+        this.registration = registration;
+    }
+    
+    public Registration AddRegistration( Registration registration , String name, String role, String username, String password, String email, String phone, String carrier,  String location, String imagePath, String status ){
+        registration = new Registration(name, role, username, password, email, phone, carrier, location, imagePath, status);
+        registrationList.add(registration);
+        
+        return registration;
+    }
+    
+    //Contructors
+    public RegistrationDirectory(){
+        this.registrationList = new ArrayList<Registration>();
+    }
     
 }
