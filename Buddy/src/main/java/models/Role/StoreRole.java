3ba75600ad0c0e98f397a6716cb59838d20aefdb
Naina Rajan <rajan.nai@northeastern.edu>
2022-04-30 20:04:13 -0400

    UI for Store Role

--- Buddy/src/main/java/models/Role/StoreRole.java
+++ Buddy/src/main/java/models/Role/StoreRole.java
@@ -5,14 +5,16 @@
 package models.Role;
 
 import javax.swing.JPanel;
+import models.AppSystem;
 import models.Person.Person;
+import ui.StoreAdminRole.ManageStoreJPanel;
 
 /**
  *
  * @author thomas
  */
-public class StoreRole {
-    public JPanel createWorkArea(JPanel userProcessContainer, Person account, System business) {
+public class StoreRole extends Role{
+    public JPanel createWorkArea(JPanel userProcessContainer, Person account, AppSystem business) {
         return new ManageStoreJPanel(userProcessContainer, account, business);
     }  
 }
